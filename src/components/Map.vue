7cc770feb8bd4fa1891c0cb9a95537c1c7e40f0f
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-12-05 17:07:41 +0100

    add dark mode map switch

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -16,17 +16,21 @@ export default {
             type: 'Feature',
             geometry: {
               type: 'Point',
-              coordinates: [51.91711547670975, 4.484114731421678]
-            },
-            properties: {
-              title: 'Racefiets Bob',
-              description: 'Laatst gezien: 1 december 2021'
+              coordinates: [0, 0]
+            }
+          },
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [100, 100]
             }
           },
         ]
       },
       mapType: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-v10' :'streets-v11',
-      map: ''
+      map: null,
+      pulsingDot: null
     };
   },
   mounted(){
@@ -43,25 +47,95 @@ export default {
       zoom: 1 // starting zoom
     });
 
-    for (const feature of this.markerData.features) {
-      // create a HTML element for each feature
-      const el = document.createElement('div');
-      el.className = 'marker';
-      const text = document.createElement('span')
-      el.appendChild(text)
-      text.className = 'text'
-      text.innerText = feature.properties.title
+    const size = 200;
+    let map = this.map;
 
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(this.map);
-    }
+    // This implements `StyleImageInterface`
+    // to draw a pulsing dot icon on the map.
+    this.pulsingDot = {
+      width: size,
+      height: size,
+      data: new Uint8Array(size * size * 4),
 
-    this.map.on('click', 'circle', (e) => {
-      this.map.flyTo({
-        center: e.features[0].geometry.coordinates
+      // When the layer is added to the map,
+      // get the rendering context for the map canvas.
+      onAdd: function () {
+        const canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+      },
+
+      // Call once before every frame where the icon will be used.
+      render: function () {
+        const duration = 1000;
+        const t = (performance.now() % duration) / duration;
+
+        const radius = (size / 2) * 0.3;
+        const outerRadius = (size / 2) * 0.7 * t + radius;
+        const context = this.context;
+
+        // Draw the outer circle.
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            outerRadius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+        context.fill();
+
+        // Draw the inner circle.
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            radius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        // Update this image's data with data from the canvas.
+        this.data = context.getImageData(
+            0,
+            0,
+            this.width,
+            this.height
+        ).data;
+
+        // Continuously repaint the map, resulting
+        // in the smooth animation of the dot.
+        map.triggerRepaint();
+
+        // Return `true` to let the map know that the image was updated.
+        return true;
+      }
+    };
+    let pulsingDot = this.pulsingDot;
+    this.map.on('load', () => {
+      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+      this.map.addSource('dot-point', {
+        'type': 'geojson',
+        'data': this.markerData
+      });
+      this.map.addLayer({
+        'id': 'layer-with-pulsing-dot',
+        'type': 'symbol',
+        'source': 'dot-point',
+        'layout': {
+          'icon-image': 'pulsing-dot'
+        }
       });
     });
-
   }
 };
 </script>
