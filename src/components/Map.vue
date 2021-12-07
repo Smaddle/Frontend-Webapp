e73f98103f3ab92cd2fc3fe906251fe523e0b01b
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-12-07 10:32:16 +0100

    fix map on mode switching

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -49,9 +49,6 @@ export default {
 
     const size = 200;
     let map = this.map;
-
-    // This implements `StyleImageInterface`
-    // to draw a pulsing dot icon on the map.
     this.pulsingDot = {
       width: size,
       height: size,
@@ -78,25 +75,13 @@ export default {
         // Draw the outer circle.
         context.clearRect(0, 0, this.width, this.height);
         context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            outerRadius,
-            0,
-            Math.PI * 2
-        );
+        context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
         context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
         context.fill();
 
         // Draw the inner circle.
         context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            radius,
-            0,
-            Math.PI * 2
-        );
+        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
         context.fillStyle = 'rgba(255, 100, 100, 1)';
         context.strokeStyle = 'white';
         context.lineWidth = 2 + 4 * (1 - t);
@@ -104,12 +89,7 @@ export default {
         context.stroke();
 
         // Update this image's data with data from the canvas.
-        this.data = context.getImageData(
-            0,
-            0,
-            this.width,
-            this.height
-        ).data;
+        this.data = context.getImageData(0, 0, this.width, this.height).data;
 
         // Continuously repaint the map, resulting
         // in the smooth animation of the dot.
@@ -119,23 +99,47 @@ export default {
         return true;
       }
     };
-    let pulsingDot = this.pulsingDot;
-    this.map.on('load', () => {
-      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+    this.map.on('style.load', () => {
+      this.addDataLayer();
+    })
+  },
 
-      this.map.addSource('dot-point', {
-        'type': 'geojson',
-        'data': this.markerData
+  methods : {
+    addDataLayer() {
+      this.map.addImage('pulsing-dot', this.pulsingDot, {pixelRatio: 2}); //this adds the custom marker to the sprites that can be used on the map
+
+      this.map.addSource('smaddle-locations', {
+        'type': 'geojson', 'data': {
+          'type': 'FeatureCollection',
+          'features': [
+            {
+              'type': 'Feature',
+              'properties': {
+                'description': 'test'
+              },
+              'geometry':
+                  {
+                    'type': 'Point',
+                    'coordinates': [5, 52] // icon position [lng, lat]
+                  }
+            }
+          ]
+        }
       });
+
       this.map.addLayer({
         'id': 'layer-with-pulsing-dot',
         'type': 'symbol',
-        'source': 'dot-point',
+        'source': 'smaddle-locations',
         'layout': {
           'icon-image': 'pulsing-dot'
         }
       });
-    });
+
+      this.map.on('click', 'layer-with-pulsing-dot', (e) => {
+        console.log(e.features[0].properties.description);
+      })
+    }
   }
 };
 </script>
