cf2faf676ec06652028675ae4879a1d8399b9ff8
Jules Immerzeel <jules.immerzeel@gmail.com>
2022-01-06 12:12:55 +0100

    code cleanup vuex

--- src/store/deviceModule.js
+++ src/store/deviceModule.js
@@ -1,89 +1,85 @@
 export const deviceModule = {
-    state: {
-        markerData: new Map([["4ea2353a-fc4d-4463-b244-1279243b4396", {
+  state: {
+    markerData: new Map([["4ea2353a-fc4d-4463-b244-1279243b4396", {
+      geoJson: {
+        type: 'Feature',
+        properties: {
+          id: 0,
+          name: 'Driewieller van Sascha',
+          last_updated: 1639405146,
+          stolen: false,
+          battery: 20,
+          DeviceToken: "4ea2353a-fc4d-4463-b244-1279243b4396"
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [5, 52]
+        }
+      },
+      marker: null
+    }]]),
+
+    webSocket: null,
+  },
+
+  mutations: {
+    selectSmaddle(state, smaddle) {
+      state.selectedSmaddle = smaddle
+    },
+
+    setWebSocket(state, socket) {
+      state.webSocket = socket
+    },
+
+    //used for setting the smaddle data from backend
+    setSmaddle(state, smaddle) {
+      state.markerData.set(smaddle.properties.DeviceToken, smaddle)
+    },
+
+    //used to process the smaddle data given by DeviceApi
+    updateGeoJson(state, socketData) {
+      socketData.forEach(smaddle => {
+        state.markerData.set(smaddle.properties.Id,
+          {
             geoJson: {
-                type: 'Feature',
-                properties: {
-                    id: 0,
-                    name: 'Driewieller van Sascha',
-                    last_updated: 1639405146,
-                    stolen: false,
-                    battery: 20,
-                    DeviceToken: "4ea2353a-fc4d-4463-b244-1279243b4396"
-                },
-                geometry: {
-                    type: 'Point',
-                    coordinates: [5, 52]
-                }
+              type: "feature",
+              properties: {
+                ...state.markerData.get(smaddle.properties.Id).geoJson.properties,
+                ...smaddle.properties
+              },
+              geometry: smaddle.geometry
             },
-            marker: null
-        }]]),
-
-        webSocket: null,
+            marker: state.markerData.get(smaddle.properties.Id).marker
+          })
+        state.markerData.get(smaddle.properties.Id).marker.setLngLat(smaddle.geometry.coordinates)
+      })
     },
 
-    mutations: {
-        selectSmaddle(state,smaddle){
-            state.selectedSmaddle = smaddle
-        },
+    setMarker(state, payload) {
+      state.markerData.get(payload.id).marker = payload.marker;
+    }
+  },
 
-        setWebSocket(state, socket){
-            state.webSocket = socket
-        },
+  actions: {
+    registerDevices({state, commit}) {
+      //Todo get the list with devices from backend user model then register those via this websocket connection
+      const deviceTokens = [];
+      for (let smaddle of state.markerData.values()) {
+        deviceTokens.push(smaddle.geoJson.properties.DeviceToken); //get all the tokens of the smaddles we want to track
+      }
 
-        //used for setting the smaddle data from backend
-        setSmaddle(state, smaddle)
-        {
-            state.markerData.set(smaddle.properties.DeviceToken, smaddle)
-        },
+      let socket = new WebSocket("ws://localhost:8888")
+      socket.onopen = () => socket.send(JSON.stringify({
+        "action": "REGISTER",
+        "instructions": [""],
+        "smaddles": deviceTokens
+      }))
 
-        //used to process the smaddle data given by DeviceApi
-        updateGeoJson(state, socketData)
-        {
-            socketData.forEach(smaddle => {
-                state.markerData.set(smaddle.properties.Id,
-                    {
-                        geoJson: {
-                            type: "feature",
-                            properties: {
-                                ...state.markerData.get(smaddle.properties.Id).geoJson.properties,
-                                ...smaddle.properties
-                            },
-                            geometry: smaddle.geometry
-                        },
-                        marker: state.markerData.get(smaddle.properties.Id).marker
-                    })
-                state.markerData.get(smaddle.properties.Id).marker.setLngLat(smaddle.geometry.coordinates)
-            })
-        },
-
-        setMarker(state, payload)
-        {
-            state.markerData.get(payload.id).marker = payload.marker;
-        }
-    },
-
-    actions: {
-        registerDevices({state, commit})
-        {
-            //Todo get the list with devices from backend user model then register those via this websocket connection
-            const deviceTokens = [];
-            for (let smaddle of state.markerData.values()) {
-                deviceTokens.push(smaddle.geoJson.properties.DeviceToken); //get all the tokens of the smaddles we want to track
-            }
-
-            let socket = new WebSocket("ws://localhost:8888")
-            socket.onopen = () => socket.send(JSON.stringify({
-                "action": "REGISTER",
-                "instructions":[""],
-                "smaddles": deviceTokens
-            }))
-
-            socket.onmessage = (e) => {
-                let data = JSON.parse(e.data).features
-                commit("updateGeoJson", data)
-            }
-            commit('setWebSocket', socket)
-        }
-    },
+      socket.onmessage = (e) => {
+        let data = JSON.parse(e.data).features
+        commit("updateGeoJson", data)
+      }
+      commit('setWebSocket', socket)
+    }
+  },
 }
--- src/store/userModule.js
+++ src/store/userModule.js
@@ -1,99 +1,94 @@
 import router from '../router'
 
 export const userModule = {
-    state: {
-        user: null,
-        loggedIn: false
+  state: {
+    user: null,
+    loggedIn: false
+  },
+
+  mutations: {
+    setUser(state, userData) {
+      state.user = userData
     },
 
-    mutations: {
-        setUser(state, userData){
-            state.user = userData
-        },
-
-        setStatus(state, value)
-        {
-            state.loggedIn = value
-        }
-    },
-
-    actions: {
-        async register(state, registerData) {
-            try {
-                let res = await fetch('http://localhost:8000/Users/register', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json, text/plain',
-                        'Content-Type': 'application/json;charset=UTF-8'
-                    },
-                    body: JSON.stringify({
-                        "Username": registerData.username,
-                        "Password": registerData.password,
-                        "EmailAddress": registerData.emailAddress,
-                        "FirstName": registerData.firstname,
-                        "MiddleName": registerData.middlename,
-                        "LastName": registerData.lastname
-                    })
-                })
-                if (res.status === 200) {
-                    await router.push({name: 'Inloggen'})
-                }
-            }
-            catch (e) {
-                console.log(e)
-            }
-        },
-
-        async login({commit}, loginData) {
-            try {
-                let res = await fetch('http://localhost:8000/Users/login', {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Accept': 'application/json, text/plain',
-                        'Content-Type': 'application/json;charset=UTF-8'
-                    },
-                    body: JSON.stringify({username: loginData.username, password: loginData.password})
-                })
-                if (res.status === 200) {
-                    commit('setUser', await res.json())
-                    commit('setStatus', true)
-                    await router.push({ name: 'Home'})
-                }
-            }
-            catch (e) {
-                console.log(e)
-            }
-        },
-
-        async updateAccount({commit}, updatedUser) {
-            try {
-                let res = await fetch('http://localhost:8000/Users/update', {
-                    method: 'PUT',
-                    credentials: 'include',
-                    headers: {
-                        'Accept': 'application/json, text/plain',
-                        'Content-Type': 'application/json;charset=UTF-8'
-                    },
-                    body: JSON.stringify({
-                        "username": updatedUser.username,
-                        "emailAddress":updatedUser.emailAddress,
-                        "firstName":updatedUser.firstName,
-                        "middleName":null,
-                        "lastName":updatedUser.lastName,
-                        "devices":updatedUser.devices
-                    })
-                })
-                if (res.status === 401) {
-                    router.push({ name: 'Inloggen'})
-                }
-                else {
-                    commit('setUser', updatedUser)
-                }
-            }
-            catch (e) {
-                console.log(e)
-            }
-        }
+    setStatus(state, value) {
+      state.loggedIn = value
     }
+  },
+
+  actions: {
+    async register(state, registerData) {
+      try {
+        let res = await fetch('http://localhost:8000/Users/register', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify({
+            "Username": registerData.username,
+            "Password": registerData.password,
+            "EmailAddress": registerData.emailAddress,
+            "FirstName": registerData.firstname,
+            "MiddleName": registerData.middlename,
+            "LastName": registerData.lastname
+          })
+        })
+        if (res.status === 200) {
+          await router.push({name: 'Inloggen'})
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    async login({commit}, loginData) {
+      try {
+        let res = await fetch('http://localhost:8000/Users/login', {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify({username: loginData.username, password: loginData.password})
+        })
+        if (res.status === 200) {
+          commit('setUser', await res.json())
+          commit('setStatus', true)
+          await router.push({name: 'Home'})
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    async updateAccount({commit}, updatedUser) {
+      try {
+        let res = await fetch('http://localhost:8000/Users/update', {
+          method: 'PUT',
+          credentials: 'include',
+          headers: {
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify({
+            "username": updatedUser.username,
+            "emailAddress": updatedUser.emailAddress,
+            "firstName": updatedUser.firstName,
+            "middleName": null,
+            "lastName": updatedUser.lastName,
+            "devices": updatedUser.devices
+          })
+        })
+        if (res.status === 401) {
+          router.push({name: 'Inloggen'})
+        } else {
+          commit('setUser', updatedUser)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
